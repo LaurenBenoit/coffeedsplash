1b2d57a0a7abe39ab0064993b548e43d8e9cef61
fahadrao <fahadrao@gmail.com>
2017-01-09 02:37:34 +0500

    commit test

--- coffedsplash/settings.py
+++ coffedsplash/settings.py
@@ -88,6 +88,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
 ALLOWED_HOSTS = ['*']
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
